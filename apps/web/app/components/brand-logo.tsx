a39c509583a5b42abc07b76076fab84a564e66c3
Levy <levy.silva@claro.com.br>
2026-09-05 10:29:40 -0300

    logo

--- apps/web/app/components/brand-logo.tsx
+++ apps/web/app/components/brand-logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../../img/guacamole_market013_preto.jpg";
+import logo from "../../img/logo-market013.jpg";
 
 export default function BrandLogo() {
   return <Image className="brand-logo" src={logo} alt="market013.app" priority />;
